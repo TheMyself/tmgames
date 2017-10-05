526a4fa0e6c74b67a04a559cdaab337f2cb7fde9
TheMyself <minekingw@gmail.com>
2017-10-05 16:49:39 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+var metallonite = 0;
+var food;
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+
+tabcontent[i].style.display = "none";
+}
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+}
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+function metalloniteClick(number){
+    metallonite = metallonite + number;
+    document.getElementById("metallonite").innerHTML = metallonite;
+};
+
+var miners = 0;
+
+function buyMiner(){
+    var minerCost = Math.floor(10 * Math.pow(1.1,miners));
+    if(metallonite >= minerCost){               
+        miners = miners + 1;                          
+    	metallonite = metallonite - minerCost;    
+                    
+    document.getElementById('miners').innerHTML = miners;
+        document.getElementById('metallonite').innerHTML = metallonite;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,miners));      
+    document.getElementById('minerCost').innerHTML = nextCost;
+};
+
+function pFoodConsume(){
+food = food - document.getElementById('pAmount').innerHTML * 0.3;
+document.getElementById('fAmount').innerHTML = Math.trunc(food);
+}
+
+function save() {
+var save = {
+metallonite: metallonite,
+food: food
+}
+localStorage.setItem("save",JSON.stringify(save));
+}
+
+function load() {
+  var savegame = JSON.parse(localStorage.getItem("save"));
+  if (typeof savegame.food !== "undefined") food = savegame.food;
+  else food=200;
+}
+
+window.setInterval(function(){
+    metalloniteClick(miners);
+    pFoodConsume();
+}, 1000);
